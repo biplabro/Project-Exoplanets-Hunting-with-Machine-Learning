3eb0d2884a7b73c3d5e071c3c6ab176c07a644a8
Biplab Roy <roy_biplab_89@yahoo.co.in>
2019-10-08 21:51:31 +0530

    Data Preprocessing

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -41,8 +41,11 @@ dfy3 = pd.DataFrame(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-
-
+#Feature scaling: putting variables in the same range despite their original values
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
  
 
 
