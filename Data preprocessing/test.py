0d1849846b9eca03ef3bcacf08cbdc302a49f86f
Biplab Roy <35135009+biplabro@users.noreply.github.com>
2021-05-28 21:01:03 +0530

    update

--- Data preprocessing/test.py
+++ Data preprocessing/test.py
@@ -7,10 +7,11 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, :-1].values #store all Independant variables
+Y = dataset.iloc[:, -1].values  #store all Dependant variables
 
 print(X)
+print(Y)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
