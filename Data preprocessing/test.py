0cd9839cc4c1d9e5d9b8baf900e0d4449048df2e
Biplab Roy <35135009+biplabro@users.noreply.github.com>
2021-06-03 21:43:52 +0530

    commit

--- Data preprocessing/test.py
+++ Data preprocessing/test.py
@@ -31,8 +31,8 @@ print(Y)
 # %%
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean') #create instance
-imputer.fit(X[:,1:3])   # Apply imputer to X dataframe, fit data into ONLY numeric data columns
-X[:,1:3] = imputer.transform(rawData)
+imputer.fit(X[:,1:3])   # Apply imputer to X dataframe
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # %% [markdown]
 # ## Print New dataset
@@ -65,17 +65,56 @@ Y = le.fit_transform(Y)
 # %%
 print(Y)
 
-
-# %%
-
-
-
-# %%
-## Splitting the dataset into the Training set and Test set
-
+# %% [markdown]
+# ## Splitting the dataset into the Training set and Test set
 
 # %%
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 1)
+
+
+# %%
+print(X_train)
+
+
+# %%
+print(X_test)
+
+
+# %%
+print(Y_train)
+
+
+# %%
+print(Y_test)
+
+# %% [markdown]
+# ## Feature Scaling (Standardization)
+
+# %%
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:,3:] = sc.fit_transform(X_train[:,3:])
+X_test[:,3:] = sc.transform(X_test[:,3:])
+
+
+# %%
+print(X_train)
+
+
+# %%
+print(X_test)
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
 
 
